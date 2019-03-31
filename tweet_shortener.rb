ea704b360b76c438985042f3e5b494f9d2fd7004
Nicholas Eubanks <nick-2013@live.com>
2019-03-31 17:30:21 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,8 +1,11 @@
 def dictionary
   words_to_be_substituted = {
     "hello" => "hi",
-    "to" => "2"
-    "two" => "2"
-    "too" => "2"
-    ["for", "four"] => "4"
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    ["for", "four"] => "4",
+    "you" => "u", 
+    "at" => "@",
+    "and" => "&"
   }
